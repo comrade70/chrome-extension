2a8fc3317cecc2376df81f9868b1be8a6e264a1e
comrade70 <ademolajoseph88@gmail.com>
2023-10-05 12:22:18 +0100

    project folder renamed

--- backend/chrome-extension/urls.py
+++ backend/chrome-extension/urls.py
@@ -1,11 +1,3 @@
-# from django.contrib import admin
-# from django.urls import path, include
-
-# urlpatterns = [
-#     path('admin/', admin.site.urls),
-#     path('api/', include('screen_recorder.urls')),
-# ]
-
 from django.urls import path, include
 # from rest_framework.routers import DefaultRouter
 # from screen_recorder.views import APIView
